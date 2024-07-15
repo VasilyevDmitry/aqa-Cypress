4ec5e6c4d2b0034c9d5b2b300818c9a9cef1b231
admin <admin@MacBook-Air.local>
2024-07-15 15:15:57 +0200

    lessonNineteenCyplress

--- cypress.config.two.js
+++ cypress.config.two.js
@@ -1,4 +1,3 @@
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -7,10 +6,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-  baseUrl:'https://guest:welcome2qauto@qauto.forstudy.space/', 
+  baseUrl:'https://guest:welcome2qauto@qauto2.forstudy.space/', 
   env: {
-    email: 'dmitryv1800@gmail.com',
-    password: 'Dmitryv1800'
+    email: 'dmelesh28@gmail.com',
+    password: 'Ddmelesh1800'
   }
   },
 });
